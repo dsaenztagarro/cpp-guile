a47511cad172b2f2fec9d3543289074b84b53ddd
David Saenz <david.saenz.tagarro@gmail.com>
2016-08-13 00:01:02 +0200

    Basic kevent integration

--- src/main.c
+++ src/main.c
@@ -8,10 +8,42 @@
 
 #pragma clang diagnostic pop
 
-#include <dirent.h>
+#include <dirent.h>    // for DIR, dirent
+#include <fcntl.h>     // for open
+#include <sys/file.h>  // for O_RDONLY
+#include <sys/event.h> // for kqueue, kevent
 
 int main ()
 {
+        int kq = kqueue ();
+
+        struct kevent direvent;
+
+        int dirfd = open("examples/tasks", O_RDONLY, 0);
+
+        unsigned int vnode_events;
+
+        vnode_events = NOTE_DELETE |  NOTE_WRITE | NOTE_EXTEND | NOTE_ATTRIB | NOTE_LINK | NOTE_RENAME | NOTE_REVOKE;
+        EV_SET (&direvent, dirfd, EVFILT_VNODE, EV_ADD | EV_CLEAR | EV_ENABLE,
+                vnode_events, 0, 0);
+
+        kevent(kq, &direvent, 1, NULL, 0, NULL);
+
+        while (1) {
+                // camp on kevent() until something interesting happens
+                struct kevent change;
+                if (kevent(kq, NULL, 0, &change, 1, NULL) == -1) { exit(1); }
+                // The signal event has NULL in the user data.  Check for that first.
+                if (change.udata == NULL) {
+                        break;
+                } else {
+                        // udata is non-null, so it's the name of the directory
+                        printf ("%s\n", (char*)change.udata);
+                }
+        }
+        close (kq);
+
+
         DIR* tasks_dir;
         struct dirent* task_file;
 
